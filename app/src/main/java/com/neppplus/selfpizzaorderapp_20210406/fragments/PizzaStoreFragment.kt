a6bad014b140b549c865e6cda3bc926671c7d3b3
전은형 <ooooo5h91@gmail.com>
2021-04-06 22:46:26 +0900

    어댑터 객체 생성

--- app/src/main/java/com/neppplus/selfpizzaorderapp_20210406/fragments/PizzaStoreFragment.kt
+++ app/src/main/java/com/neppplus/selfpizzaorderapp_20210406/fragments/PizzaStoreFragment.kt
@@ -6,12 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.neppplus.selfpizzaorderapp_20210406.R
+import com.neppplus.selfpizzaorderapp_20210406.adapters.StoreAdapter
 import com.neppplus.selfpizzaorderapp_20210406.datas.Store
 
 class PizzaStoreFragment : Fragment() {
 
 //    피자가게 정보를 담아둘 ArrayList 변수 추가
     val mPizzaStoreList = ArrayList<Store>()
+
+//   xml + 데이터를 조합해서 뿌려주는 어댑터 변수를 추가
+    lateinit var mPizzaStoreAdapter : StoreAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +31,11 @@ class PizzaStoreFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
     mPizzaStoreList.add(Store("피자헛", "URL", "전화번호"))
+
+//    m어댑터의 대입을 진행했음. mContext가 아니라 화면이니까 액티비티고, 가게정보를 띄우는거니까 스토러리스트!!
+    mPizzaStoreAdapter = StoreAdapter(activity!!, R.layout.store_list_item, mPizzaStoreList)
+
+
 
 
 
