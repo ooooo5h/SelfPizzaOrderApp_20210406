b391a1749487d34cb87781745742cfd1c91cab5a
전은형 <ooooo5h91@gmail.com>
2021-04-06 22:38:13 +0900

    xml를 코틀린으로 꺼내주자. layoutinflator

--- app/src/main/java/com/neppplus/selfpizzaorderapp_20210406/adapters/StoreAdapter.kt
+++ app/src/main/java/com/neppplus/selfpizzaorderapp_20210406/adapters/StoreAdapter.kt
@@ -1,7 +1,9 @@
 package com.neppplus.selfpizzaorderapp_20210406.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.widget.ArrayAdapter
+import com.neppplus.selfpizzaorderapp_20210406.R
 import com.neppplus.selfpizzaorderapp_20210406.datas.Store
 
 // 피자 가게 데이터를 만들었자나? 그럼 이제 그 데이터를 또 뿌려줄 어댑터를 만들어야함.
@@ -10,4 +12,7 @@ class StoreAdapter(
     val mContext : Context,
     val resId : Int,
     val mList : ArrayList<Store>) : ArrayAdapter<Store>(mContext, resId, mList) {
+
+//    xml을 코틀린변수로 꺼내오도록 도와주는 클래스. LayoutInflater... = 이거 어렵군
+    val inflater = LayoutInflater.from(mContext)
 }
